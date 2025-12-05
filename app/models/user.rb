b1a087ff27935c67a3ff7547ccb512763ee40ee0
mattilagan1 <ilagan.matthew67@gmail.com>
2025-12-05 11:15:52 +1000

    created user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :recipes, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+end
